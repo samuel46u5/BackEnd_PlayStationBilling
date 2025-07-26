5571484c8c821f188136c4578873a1ee0b952664
samuel46u5 <samuel_46u5@yahoo.com>
2025-07-26 12:03:47 +0700

    ok

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { exec } = require("child_process");
 const app = express();
-const port = 3002;
+const port = 3001;
 
 app.use(cors());
 
